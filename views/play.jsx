644f3b9b39b75e10f5fc49425c09cfc521248130
Mal F <malfellows13@gmail.com>
2022-08-18 19:53:10 -0500

    added game to react window play.jsx

--- views/play.jsx
+++ views/play.jsx
@@ -2,11 +2,16 @@
 const React = require('react');
 const Def = require('./default');
 
+// https://stackoverflow.com/questions/38497334/how-to-run-html-file-on-localhost
+// Instructions for hosting game locally. Make sure to use http://localhost:8080/ and 
+// run the the command "http-server" inside of the game file in the terminal 
+
 function play(){
     return (
         <Def>
             <main>
                 <p>Game window goes here?</p>
+                <div dangerouslySetInnerHTML={{ __html: "<iframe src='http://localhost:8080/' />"}} />
             </main>
         </Def>
     )
