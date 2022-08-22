bdac8711031074cad39f0e9baa992ba565f6dcf6
tjs6595 <t.schultejans@schenckprocess.com>
2022-08-22 06:52:20 -0500

    Fixed MongoDB schema-removed required fields and enumerations.

--- mongo_models/pokemon.js
+++ mongo_models/pokemon.js
@@ -10,18 +10,18 @@ const pokemonSchema = new Schema({
     image: { type: String, default: '' },
     sprite: { type: String, default: '' },
     level: {type: Number, default: 1 },
-    species: {type: String, required: true, enum: ['Caterpie', 'Pidgey', 'Rattata', 'Charmander', 'Spearow', 'Squirtle' ]},
-    type_1: {type: String, required: true, enum: ['Normal', 'Fire', 'Water', 'Grass', 'Flying', 'Bug', 'Electric' ]},
-    type_2: {type: String, enum: ['Normal', 'Fire', 'Water', 'Grass', 'Flying', 'Bug', 'Electric' ]},
-    ability: {type: String, required: true, enum: ['Torrent', 'Blaze', 'Keen Eye', 'Run Away', 'Shield Dust' ]},
+    species: {type: String},
+    type_1: {type: String},
+    type_2: {type: String},
+    ability: {type: String},
     dex_entry: {type: String},
     hitpoints: {type: Number, default: 5 },
     defense: {type: Number, default: 5 },
     player: {type: String },
-    move_1: {type: String, required: true, enum: ['Tackle', 'String Shot', 'Sand Attack', 'Tail Whip', 'Quick Attack', 'Scratch', 'Growl', 'Peck', 'Leer' ]},
-    move_2: {type: String, enum: ['Tackle', 'String Shot', 'Sand Attack', 'Tail Whip', 'Quick Attack', 'Scratch', 'Growl', 'Peck', 'Leer' ]},
-    move_3: {type: String, enum: ['Tackle', 'String Shot', 'Sand Attack', 'Tail Whip', 'Quick Attack', 'Scratch', 'Growl', 'Peck', 'Leer' ]},
-    move_4: {type: String, enum: ['Tackle', 'String Shot', 'Sand Attack', 'Tail Whip', 'Quick Attack', 'Scratch', 'Growl', 'Peck', 'Leer' ]}
+    move_1: {type: String},
+    move_2: {type: String},
+    move_3: {type: String},
+    move_4: {type: String}
 })
 
 // MODEL AND EXPORT
