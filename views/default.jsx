424ad625b8698b686d9a957c6fc8a431630d0ac1
Mal F <malfellows13@gmail.com>
2022-08-21 22:21:55 -0500

    minor bug fixes

--- views/default.jsx
+++ views/default.jsx
@@ -15,7 +15,7 @@ function Def(html){
                 {/* Main CSS */}
                 <link rel='stylesheet' href='/css/style.css'/>
                 <script src="https://use.fontawesome.com/cc7c57117a.js"></script>
-                <script src="https://kit.fontawesome.com/c5973c0bc5.js" crossorigin="anonymous"></script>
+                <script src="https://kit.fontawesome.com/c5973c0bc5.js" crossOrigin="anonymous"></script>
             </head>
             {/* NAVBAR */}
             <nav>
@@ -28,7 +28,7 @@ function Def(html){
                         <a href='/instructions'><i className="fa fa-question-circle" aria-hidden="true"></i> Instructions</a>
                     </li>
                     <li>
-                        <a href='/play'><i class="fa-solid fa-fire"></i>Play</a>
+                        <a href='/play'><i className="fa-solid fa-fire" aria-hidden="true"></i>Play</a>
                     </li>
                     <li>
                         <a href='/mockmon'>Mockdex</a>
--- views/mockmon/index.jsx
+++ views/mockmon/index.jsx
@@ -12,25 +12,23 @@ function index(data) {
     let pokeFormatted = data.pokemon.map((pokemon) => {
         return (
             <div>
-                <div>
-                    <h3>{pokemon.species}</h3>
-                    <img src={pokemon.image} alt={pokemon.name}></img>
-                    <img src={pokemon.sprite} alt={pokemon.sprite}></img>
-                    <p>Lv. {pokemon.level}</p>
-                    <ul>Type(s): 
-                    <li>{pokemon.type_1}</li> 
-                    <li>{pokemon.type_2}</li></ul>
-                    <p>Ability: {pokemon.ability}</p>
-                    <p>Entry: {pokemon.dex_entry}</p>
-                    <p>Hit Points: {pokemon.hit_points}</p>
-                    <p>Defense: {pokemon.defense}</p>
-                    <ul>Move(s)
-                        <li>{pokemon.move_1}</li>
-                        <li>{pokemon.move_2}</li>
-                        <li>{pokemon.move_3}</li>
-                        <li>{pokemon.move_4}</li>
-                    </ul>
-                </div>
+                <h3>{pokemon.species}</h3>
+                <img src={pokemon.image} alt={pokemon.name}></img>
+                <img src={pokemon.sprite} alt={pokemon.sprite}></img>
+                <p>Lv. {pokemon.level}</p>
+                <ul>Type(s): 
+                <li>{pokemon.type_1}</li> 
+                <li>{pokemon.type_2}</li></ul>
+                <p>Ability: {pokemon.ability}</p>
+                <p>Entry: {pokemon.dex_entry}</p>
+                <p>Hit Points: {pokemon.hit_points}</p>
+                <p>Defense: {pokemon.defense}</p>
+                <ul>Move(s)
+                    <li>{pokemon.move_1}</li>
+                    <li>{pokemon.move_2}</li>
+                    <li>{pokemon.move_3}</li>
+                    <li>{pokemon.move_4}</li>
+                </ul>
             </div>
         )
     })
@@ -40,7 +38,10 @@ function index(data) {
     <Def>
         <main>
             <h1>Mockdex</h1>  
-            {pokeFormatted}        
+            <div className='row'>
+                {pokeFormatted}  
+            </div>
+                  
         </main>
     </Def>
     )
